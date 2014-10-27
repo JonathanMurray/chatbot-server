e00cb86dca71b1509d227efe0d9545511229fcd3
JonathanMurray <jonte_murray@hotmail.com>
2014-10-27 22:46:51 +0100

    enabled access from other sites on the same server

--- server.pl
+++ server.pl
@@ -1,21 +1,20 @@
-father(bertil, jonathan).
-father(bertil, josef).
-father(c-a, bertil).
-
-
 :- use_module(library(http/thread_httpd)).
 :- use_module(library(http/http_dispatch)).
 :- use_module(library(http/http_error)).
 :- use_module(library(http/html_write)).
 :- use_module(library(http/http_parameters)).
+:- use_module(library(http/http_cors)).
 
 :- http_handler('/', reply, []).
 
+:- set_setting_default(http:cors, [*]).
 
 server(Port) :-
     http_server(http_dispatch, [port(Port)]).
 
 reply(Request) :-
+	cors_enable,
+
 	consult(nlp/main),
 	catch(
 	     http_parameters(Request,
